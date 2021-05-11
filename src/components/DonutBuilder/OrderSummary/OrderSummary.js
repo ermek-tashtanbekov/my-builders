a74de010bf9be737fc2a6636af70fd17c158e2fa
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 19:29:09 +0600

    create OrderSummary

---
+++ src/components/DonutBuilder/OrderSummary/OrderSummary.js
@@ -0,0 +1,25 @@
+import classes from "./OrderSummary.module.css";
+
+const OrderSummary = ({ ingredients, price }) => {
+    const labels = {
+        red: "Red",
+        blue: "Blue",
+        brown: "Brown",
+        black: "Black",
+        pink: "Pink",
+        violet: "Violet"
+    }
+    const results = Object.keys(ingredients)
+        .map(type => <li>{labels[type]} : {ingredients[type]}</li>)
+    return (
+        <div className={classes.OrderSummary}>
+            <h3>Order summary</h3>
+            <ul>
+                {results}
+            </ul>
+            <strong>Total price: {price.toFixed(1)} som</strong>
+        </div>
+    );
+}
+
+export default OrderSummary;
