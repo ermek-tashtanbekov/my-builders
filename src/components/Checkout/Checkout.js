32340452dc3010aad2c2438c53d5077b4a0509ab
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 20:47:39 +0600

    Add Firebase to checkoutform

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,24 +1,48 @@
 import classes from "./Checkout.module.css";
 import DonutPreview from "../DonutBuilder/DonutPreview/DonutPreview"
 import CheckoutForm from "./CheckoutForm/CheckoutForm"
+import axios from "axios";
 
-const Checkout  = ({history}) => {
-    function cancelCallback(){
+const Checkout = ({ history }) => {
+    function cancelCallback() {
         history.replace('/')
     }
-    return ( 
+    function submitCallback(event) {
+        const data = new FormData(event.target);
+
+        axios.post('https://work-1-b6be6-default-rtdb.firebaseio.com/orders.json', {
+            name: data.get('name'),
+            address: data.get('address'),
+            phone: data.get('phone'),
+            ingredients: {
+                tomato: 10,
+                salami: 10,
+                redPepper: 10,
+                yellowPepper: 10,
+                greenOlive: 10,
+                blackOlive: 10,
+            },
+            price: 100,
+        }).then(response => {
+            history.replace('/');
+        });
+
+        event.preventDefault();
+    }
+    return (
         <div className={classes.Checkout}>
             <DonutPreview ingredients={{
-                red:1,
-                blue:1,
+                red: 1,
+                blue: 1,
                 brown: 1,
-                black:1,
-                pink:1,
-                violet:1
-            }} price={150}/>
-            <CheckoutForm/>
+                black: 1,
+                pink: 1,
+                violet: 1
+            }} price={150} />
+            <CheckoutForm cancelCallback={cancelCallback}
+                submitCallback={submitCallback} />
         </div>
-     );
+    );
 }
- 
-export default Checkout ;
+
+export default Checkout;
