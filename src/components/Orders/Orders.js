4606e3d7f4f3720dfad96360964771b6d9a8a442
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 22:22:48 +0600

    use axios get

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,7 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Order from "./Order/Order";
+
 const Orders = () => {
-    return ( 
-        <div>Order</div>
-     );
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://work-1-b6be6-default-rtdb.firebaseio.com/orders.json')
+            .then(response => {
+                const newOrders = Object.keys(response.data).map(id => {
+                    return {
+                        ...response.data[id],
+                        id: id,
+                    };
+                });
+                setOrders(newOrders);
+            });
+    }, []);
+
+    const results = orders.map(order => <Order key={order.id} {...order} />);
+
+    return (
+        <div>
+            {results.length > 0 ? results : "loading"}
+        </div>
+    );
 }
  
 export default Orders;
