d44047ebac76bcdebe58fa0dbaeff3b1d47749a1
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 15:51:06 +0600

    Randomly generate ingredient position

--- src/components/DonutBuilder/DonutIngredient/DonutIngredient.js
+++ src/components/DonutBuilder/DonutIngredient/DonutIngredient.js
@@ -4,6 +4,7 @@ import brown from '../../../img/brown.svg'
 import black from '../../../img/black.svg'
 import pink from '../../../img/pink.svg'
 import violet from '../../../img/violet.svg'
+import classes from './DonutIngredient.module.css'
 
 
 const DonutIngredient = ({type}) => {
@@ -44,8 +45,39 @@ const DonutIngredient = ({type}) => {
     
     
       }
+      function getPosition(ingredientWidth) {
+        const dounutDiameter = 400;
+        const dounutRadius = dounutDiameter / 2;
+        const ingredientRadius = parseInt(ingredientWidth) / 2;
+    
+        const ingredientTop = Math.round(Math.random() * dounutDiameter);
+        const ingredientLeft = Math.round(Math.random() * dounutDiameter);
+    
+        const distance = Math.sqrt(
+          Math.pow(ingredientTop - dounutRadius, 2) + Math.pow(ingredientLeft - dounutRadius, 2)
+        ) + ingredientRadius;
+    
+        return distance < dounutRadius
+          ? {
+            top: ingredientTop - ingredientRadius,
+            left: ingredientLeft - ingredientRadius
+          }
+          : getPosition(ingredientWidth);
+      }
+    
+      // Get random position for this ingredient.
+    //   if (!fixed) {
+        const position = getPosition(types[type].width);
+        types[type].top = position.top + "px";
+        types[type].left = position.left + "px";
+    
+        // Get random rotation for this ingredient.
+        types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
+    //   }
+    
+
     return ( 
-        <div style={types[type]}></div>
+        <div className={classes.DonutIngredient} style={types[type]}></div>
      );
 }
  
