90f18361ac27ae3e7560b88012cc779b97aa9a75
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 15:41:03 +0600

    Use ingredeints constant to control ingredients

--- src/components/DonutBuilder/DonutBuilder.js
+++ src/components/DonutBuilder/DonutBuilder.js
@@ -3,9 +3,18 @@ import DonutControls from "./DonutControls/DonutControls";
 import DonutPreview from "./DonutPreview/DonutPreview";
 
 const DonutBuilder = () => {
+const ingredients ={
+    red: 1,
+    blue:1,
+    black:1,
+    pink:1,
+    brown:1,
+    violet:1
+}
+
     return ( 
         <div className={classes.DonutBuilder}>
-            <DonutPreview/>
+            <DonutPreview ingredients={ingredients}/>
             <DonutControls/>
         </div>
      );
--- src/components/DonutBuilder/DonutPreview/DonutPreview.js
+++ src/components/DonutBuilder/DonutPreview/DonutPreview.js
@@ -1,10 +1,17 @@
 import classes from './DonutPreview.module.css';
 import plate from '../../../img/plate.png'
-const DonutPreview = () => {
+const DonutPreview = ({ingredients}) => {
+    const result = []
+
+    for(const ingredient in ingredients){
+        for(let i = 0 ; i < ingredients[ingredient]; i++){
+            result.push(ingredient)
+        }
+    }
     return ( 
         <div className={classes.DonutPreview} >
             <div className={classes.plate}  style={{ backgroundImage: `url(${plate})`, backgroundPosition: "center", backgroundSize: 'cover', witdth: "520px", height: "400px", marginRight: "-3px" }}>
-                
+                <div>{result}</div>
             </div>
         </div>
      );
