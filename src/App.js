77d0a0dc3823a533fb464936f1e144a97e542a53
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 15:55:41 +0600

    Clean App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import DonutBuilder from './components/DonutBuilder/DonutBuilder';
 import Layout from './components/Layout/Layout';
 
-function App() {
-  return (
-    <div className="App">
-     <Layout>
-       <DonutBuilder/>
-     </Layout>
-    </div>
-  );
-}
+
+  const App = () =>{
+    return (
+      <div className="App">
+       <Layout>
+         <DonutBuilder/>
+       </Layout>
+      </div>
+    );
+  }
+ 
+
 
 export default App;
