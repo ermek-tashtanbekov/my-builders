d98dfffff4fc6bacfe79e38b91bb5c398cce11cd
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 22:21:21 +0600

    delete some function and axios

--- src/components/DonutBuilder/DonutBuilder.js
+++ src/components/DonutBuilder/DonutBuilder.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { load } from "../../store/action/builder";
 import Button from "../UI/Button/Button";
 import Modal from "../UI/Modal/Modal";
 import classes from "./DonutBuilder.module.css";
@@ -7,32 +9,30 @@ import DonutControls from "./DonutControls/DonutControls";
 import DonutPreview from "./DonutPreview/DonutPreview";
 import OrderSummary from "./OrderSummary/OrderSummary";
 
-const DonutBuilder = ({history}) => {
-    const [ingredients, setIngredients] = useState({})
-    const [price, setPrice] = useState(0);
+const DonutBuilder = ({ history }) => {
+    const dispatch = useDispatch();
+    const ingredients = useSelector(state => state.builder.ingredients);
+    const price = useSelector(state => state.builder.price);
     const [ordering, setOrdering] = useState(false);
-    const prices = {
-        red: 15,
-        blue: 25,
-        brown: 53,
-        black: 32,
-        pink: 25,
-        violet: 47
-    }
-    useEffect(loadDefaults, []);
 
-        function loadDefaults() {
-            axios
-                .get('https://work-1-b6be6-default-rtdb.firebaseio.com/defauld.json')
-                .then(response => {
-                    setPrice(response.data.price);
-    
-                    // For arrays
-                    // setIngredients(Object.values(response.data.ingredients));
-                    // For objects
-                    setIngredients(response.data.ingredients);
-                });
-        }
+
+    useEffect(() => dispatch(load()), []);
+
+
+    // useEffect(loadDefaults, []);
+
+    //     function loadDefaults() {
+    //         axios
+    //             .get('https://work-1-b6be6-default-rtdb.firebaseio.com/defauld.json')
+    //             .then(response => {
+    //                 setPrice(response.data.price);
+
+    //                 // For arrays
+    //                 // setIngredients(Object.values(response.data.ingredients));
+    //                 // For objects
+    //                 setIngredients(response.data.ingredients);
+    //             });
+    //     }
 
 
 
@@ -46,49 +46,25 @@ const DonutBuilder = ({history}) => {
         setOrdering(false);
     }
     function finishOrdering() {
-        axios.post('https://work-1-b6be6-default-rtdb.firebaseio.com/orders.json', {
-            ingredients: ingredients,
-            price: price,
-            address: "1234 Jusaeva str",
-            phone: "0 777 777 777",
-            name: "Sadyr Japarov",
-        })
-            .then(() => {
-                setOrdering(false);
-                loadDefaults()
-                history.push('/checkout');
-            })
+        setOrdering(false);
+        // loadDefaults()
+        history.push('/checkout');
     }
 
-    function addIngredient(type) {
-        const newIngredients = { ...ingredients };
-        newIngredients[type]++;
-        setPrice(price + prices[type]);
-        setIngredients(newIngredients);
-    }
-    function removeIngredient(type) {
-        if (ingredients[type]) {
-            const newIngredients = { ...ingredients };
-            newIngredients[type]--;
-            setPrice(price - prices[type])
-            setIngredients(newIngredients)
-        }
 
-    }
 
     return (
         <div className={classes.DonutBuilder}>
             <DonutPreview ingredients={ingredients}
                 price={price} />
             <DonutControls ingredients={ingredients}
-                addIngredient={addIngredient}
-                removeIngredient={removeIngredient}
                 startOrdering={startOrdering}
             />
 
 
             <Modal show={ordering} cancel={stopOrdering}>
-                <OrderSummary ingredients={ingredients} price={price} />
+                <OrderSummary ingredients={ingredients}
+                    price={price} />
 
                 <div className={classes.Button}>
                     <Button onClick={() => finishOrdering()} green>Checkout</Button>
@@ -100,6 +76,114 @@ const DonutBuilder = ({history}) => {
 }
 
 export default DonutBuilder;
+
+
+
+
+
+
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import Button from "../UI/Button/Button";
+// import Modal from "../UI/Modal/Modal";
+// import classes from "./DonutBuilder.module.css";
+// import DonutControls from "./DonutControls/DonutControls";
+// import DonutPreview from "./DonutPreview/DonutPreview";
+// import OrderSummary from "./OrderSummary/OrderSummary";
+
+// const DonutBuilder = ({history}) => {
+//     const [ingredients, setIngredients] = useState({})
+//     const [price, setPrice] = useState(0);
+//     const [ordering, setOrdering] = useState(false);
+//     const prices = {
+//         red: 15,
+//         blue: 25,
+//         brown: 53,
+//         black: 32,
+//         pink: 25,
+//         violet: 47
+//     }
+//     useEffect(loadDefaults, []);
+
+//         function loadDefaults() {
+//             axios
+//                 .get('https://work-1-b6be6-default-rtdb.firebaseio.com/defauld.json')
+//                 .then(response => {
+//                     setPrice(response.data.price);
+
+//                     // For arrays
+//                     // setIngredients(Object.values(response.data.ingredients));
+//                     // For objects
+//                     setIngredients(response.data.ingredients);
+//                 });
+//         }
+
+
+
+
+
+//     function startOrdering() {
+//         setOrdering(true);
+//     }
+
+//     function stopOrdering() {
+//         setOrdering(false);
+//     }
+//     function finishOrdering() {
+//         axios.post('https://work-1-b6be6-default-rtdb.firebaseio.com/orders.json', {
+//             ingredients: ingredients,
+//             price: price,
+//             address: "1234 Jusaeva str",
+//             phone: "0 777 777 777",
+//             name: "Sadyr Japarov",
+//         })
+//             .then(() => {
+//                 setOrdering(false);
+//                 loadDefaults()
+//                 history.push('/checkout');
+//             })
+//     }
+
+//     function addIngredient(type) {
+//         const newIngredients = { ...ingredients };
+//         newIngredients[type]++;
+//         setPrice(price + prices[type]);
+//         setIngredients(newIngredients);
+//     }
+//     function removeIngredient(type) {
+//         if (ingredients[type]) {
+//             const newIngredients = { ...ingredients };
+//             newIngredients[type]--;
+//             setPrice(price - prices[type])
+//             setIngredients(newIngredients)
+//         }
+
+//     }
+
+//     return (
+//         <div className={classes.DonutBuilder}>
+//             <DonutPreview ingredients={ingredients}
+//                 price={price} />
+//             <DonutControls ingredients={ingredients}
+//                 addIngredient={addIngredient}
+//                 removeIngredient={removeIngredient}
+//                 startOrdering={startOrdering}
+//             />
+
+
+//             <Modal show={ordering} cancel={stopOrdering}>
+//                 <OrderSummary ingredients={ingredients} price={price} />
+
+//                 <div className={classes.Button}>
+//                     <Button onClick={() => finishOrdering()} green>Checkout</Button>
+//                     <Button onClick={() => stopOrdering()} >Cancel</Button>
+//                 </div>
+//             </Modal>
+//         </div>
+//     );
+// }
+
+// export default DonutBuilder;
 
 
 
