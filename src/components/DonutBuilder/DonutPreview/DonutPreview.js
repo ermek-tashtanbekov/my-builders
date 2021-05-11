9e42475b13d0920899515a2795f78d931a8d0ab8
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 15:53:02 +0600

    added classes and styles

--- src/components/DonutBuilder/DonutPreview/DonutPreview.js
+++ src/components/DonutBuilder/DonutPreview/DonutPreview.js
@@ -12,7 +12,7 @@ const DonutPreview = ({ingredients}) => {
     return ( 
         <div className={classes.DonutPreview} >
             <div className={classes.plate}  style={{ backgroundImage: `url(${plate})`, backgroundPosition: "center", backgroundSize: 'cover', witdth: "520px", height: "400px", marginRight: "-3px" }}>
-                <div>{result}</div>
+                <div className={classes.ingredients }>{result}</div>
             </div>
         </div>
      );
