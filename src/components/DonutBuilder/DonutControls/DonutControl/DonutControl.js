32fe07c8ddf42654343ba4177007e0adb848bc3c
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 22:22:03 +0600

    delete function adddIngrediesnts and removeingredinens

--- src/components/DonutBuilder/DonutControls/DonutControl/DonutControl.js
+++ src/components/DonutBuilder/DonutControls/DonutControl/DonutControl.js
@@ -1,13 +1,16 @@
 import classes from "./DonutControl.module.css";
 import DonutIngredient from "../../DonutIngredient/DonutIngredient";
 import Button from "../../../UI/Button/Button";
+import { useDispatch } from "react-redux";
+import { add, remove } from "../../../../store/action/builder";
 
-const DonutControl = ({ type, addIngredient,  removeIngredient,  count }) => {
+const DonutControl = ({ type,  count }) => {
+    const dispatch = useDispatch()
     return (
         <div className={classes.DonutControl}>
-            <Button onClick={() => addIngredient(type)}>+</Button>
+            <Button onClick={() => dispatch(add(type))}>+</Button>
             <div className={classes.ingredient}><DonutIngredient type={type} fixed/></div>
-            <Button onClick={() => removeIngredient(type)} disabled={!count}>-</Button>
+            <Button onClick={() => dispatch(remove(type))} disabled={!count}>-</Button>
         </div>
     );
 }
--- src/components/DonutBuilder/DonutControls/DonutControls.js
+++ src/components/DonutBuilder/DonutControls/DonutControls.js
@@ -2,12 +2,12 @@ import Button from "../../UI/Button/Button";
 import DonutControl from "./DonutControl/DonutControl";
 import classes from "./DonutControls.module.css";
 
-const DonutControls = ({ingredients, addIngredient,removeIngredient, startOrdering }) => {
+const DonutControls = ({ingredients, startOrdering }) => {
     const results = [];
      let total = 0;
     for (const ingredient in ingredients) {
         total += ingredients[ingredient]
-         results.push(<DonutControl type={ingredient} addIngredient={addIngredient}  removeIngredient={ removeIngredient}  count={ingredients[ingredient]}/>)
+         results.push(<DonutControl type={ingredient}  count={ingredients[ingredient]}/>)
     }
         
     return ( 
@@ -24,3 +24,31 @@ const DonutControls = ({ingredients, addIngredient,removeIngredient, startOrderi
 }
  
 export default DonutControls;
+
+
+// import Button from "../../UI/Button/Button";
+// import DonutControl from "./DonutControl/DonutControl";
+// import classes from "./DonutControls.module.css";
+
+// const DonutControls = ({ingredients, addIngredient,removeIngredient, startOrdering }) => {
+//     const results = [];
+//      let total = 0;
+//     for (const ingredient in ingredients) {
+//         total += ingredients[ingredient]
+//          results.push(<DonutControl type={ingredient} addIngredient={addIngredient}  removeIngredient={ removeIngredient}  count={ingredients[ingredient]}/>)
+//     }
+        
+//     return ( 
+//         <div className={classes.DonutControls}>
+//            <div>
+//            {results}
+//            <div className={classes.Button}>
+//            <Button onClick={() => startOrdering()} disabled={!total}>Order</Button>
+//            </div>
+           
+//            </div> 
+//         </div>
+//      );
+// }
+ 
+// export default DonutControls;
