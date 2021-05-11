bd9ad35f8be7a5e37024f27dabf5e726d65db681
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 23:00:53 +0600

    added exact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Redirect, Route, Switch } from 'react-router';
 import './App.css';
 import Checkout from './components/Checkout/Checkout';
+import CheckoutForm from './components/Checkout/CheckoutForm/CheckoutForm';
 import DonutBuilder from './components/DonutBuilder/DonutBuilder';
 import Layout from './components/Layout/Layout';
 import Orders from './components/Orders/Orders';
@@ -13,7 +14,7 @@ import Orders from './components/Orders/Orders';
        <Layout>
          {/* <DonutBuilder/> */}
          <Switch>
-           <Route path="/" component={DonutBuilder}/>
+           <Route path="/" component={DonutBuilder} exact/>
            <Route path="/checkout" component={Checkout}/>
            <Route path="/orders" component={Orders}/>
            <Redirect to="/"/>
