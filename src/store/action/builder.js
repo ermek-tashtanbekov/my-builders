29d2c3a42a1d8e1862a2fc6319856f84559312b8
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 22:23:27 +0600

    new components Store the are

---
+++ src/store/action/builder.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { ADD_INGREDIENT, REMOVE_INGREDIENT, SET_INGREDIENTS } from "./types";
+
+
+export const add = (ingredient) => ({
+  type: ADD_INGREDIENT,
+  ingredient: ingredient
+});
+
+export const remove = (ingredient) => ({
+  type: REMOVE_INGREDIENT,
+  ingredient: ingredient
+});
+
+export const set = (data) => ({
+  type: SET_INGREDIENTS,
+  data: data
+});
+
+export const load = () => {
+  return (dispatch) => axios.get("https://work-1-b6be6-default-rtdb.firebaseio.com/defauld.json")
+    .then(response => dispatch(set(response.data)))
+};
---
+++ src/store/reducers/builder.js
@@ -0,0 +1,41 @@
+import { ADD_INGREDIENT, REMOVE_INGREDIENT, SET_INGREDIENTS } from "../action/types";
+
+const initialState = {
+    ingredients: {
+    },
+    price: 0,
+  };
+  const prices = {
+    tomato: 3.5,
+    salami: 4,
+    greenOlive: .3,
+    blackOlive: .3,
+    redPepper: 2,
+    yellowPepper: 1,
+  };
+  
+  const builder = (state = initialState, action) => {
+    const newState = { ...state };
+  
+    switch (action.type) {
+      case ADD_INGREDIENT:
+        newState.ingredients[action.ingredient]++;
+        newState.price += prices[action.ingredient];
+        break;
+      case REMOVE_INGREDIENT:
+        newState.ingredients[action.ingredient]--;
+        newState.price -= prices[action.ingredient];
+        break;
+      case SET_INGREDIENTS:
+        newState.ingredients = action.data.ingredients;
+        newState.price = action.data.price;
+        break;
+    
+      default:
+        break;
+    }
+  
+    return newState;
+  }
+  
+  export default builder;
