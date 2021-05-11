29af071aa9e46289366064db294e9193440b507f
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-11 22:20:24 +0600

    use react-redux and thunk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import App from './App';
+import thunk from 'redux-thunk';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider  } from 'react-redux';
+import builder from './store/reducers/builder' 
+import orders from './store/reducers/orders'
 
+const rootReducer  = combineReducers({ builder, orders });
+const store = createStore(rootReducer,applyMiddleware(thunk) )
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+
 
   </React.StrictMode>,
   document.getElementById('root')
