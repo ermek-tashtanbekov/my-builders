f36c5d81b1a1b8d09668fe64dadc791af9ec89d0
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-12 08:46:22 +0600

    test

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -5,13 +5,15 @@ import Order from "./Order/Order";
 import withAxios from "../withAxios";
 import { load } from "../../store/action/builder";
 
-const Orders = () => {
-    const orders = useSelector(state => state.orders);
-    const dispatch = useDispatch()
 
-    useEffect(() => {
-        dispatch(load())
-    }, []);
+const Orders = () => {
+    const dispatch = useDispatch();
+    const orders = useSelector(state => state.orders);
+   
+
+ useEffect(() =>{
+     dispatch(load())
+ }, [])
 
     const results = orders.map(order => <Order key={order.id} {...order} />);
 
