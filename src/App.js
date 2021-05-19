f85110d068c5569bf2ad3624f1083f399d19ee03
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-19 18:01:16 +0600

    Add isAuthenticated to App.js and Nav.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router';
 import './App.css';
 import Auth from './components/Auth/Auth';
@@ -7,24 +9,41 @@ import DonutBuilder from './components/DonutBuilder/DonutBuilder';
 import Layout from './components/Layout/Layout';
 import Logout from './components/Logout/Logout';
 import Orders from './components/Orders/Orders';
-import Loading from './Loading/Loading';
+import { restore } from './store/action/auth';
+
 
 
 
   const App = () =>{
+    const dispatch = useDispatch();
+    const isAuthenticated = useSelector(state => state.auth.token !== null);
+  
+    useEffect(() => restore(dispatch), [dispatch]);
+  
+    let routesOutput = (
+      <Switch>
+        <Route path="/" exact component={DonutBuilder} />
+        <Route path="/auth" component={Auth} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if (isAuthenticated) {
+      routesOutput = (
+        <Switch>
+          <Route path="/" exact component={DonutBuilder} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
     return (
       <div className="App">
        <Layout>
-         {/* <DonutBuilder/> */}
-         <Switch>
-           <Route path="/" component={DonutBuilder} exact/>
-           <Route path="/checkout" component={Checkout}/>
-           <Route path="/orders" component={Orders}/>
-           <Route path="/auth" component={Auth} />
-           <Route path="/logout" component={Logout} />
-           <Route path="/s" component={Loading}/>
-           <Redirect to="/"/>
-         </Switch>
+       {routesOutput}
        </Layout>
       </div>
     );
@@ -33,3 +52,52 @@ import Loading from './Loading/Loading';
 
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Redirect, Route, Switch } from 'react-router';
+// import './App.css';
+// import Auth from './components/Auth/Auth';
+// import Checkout from './components/Checkout/Checkout';
+// import DonutBuilder from './components/DonutBuilder/DonutBuilder';
+// import Layout from './components/Layout/Layout';
+// import Logout from './components/Logout/Logout';
+// import Orders from './components/Orders/Orders';
+// import Loading from './Loading/Loading';
+
+
+
+//   const App = () =>{
+//     return (
+//       <div className="App">
+//        <Layout>
+//          {/* <DonutBuilder/> */}
+//          <Switch>
+//            <Route path="/" component={DonutBuilder} exact/>
+//            <Route path="/checkout" component={Checkout}/>
+//            <Route path="/orders" component={Orders}/>
+//            <Route path="/auth" component={Auth} />
+//            <Route path="/logout" component={Logout} />
+//            <Route path="/s" component={Loading}/>
+//            <Redirect to="/"/>
+//          </Switch>
+//        </Layout>
+//       </div>
+//     );
+//   }
+ 
+
+
+// export default App;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,48 @@
+import { useSelector } from "react-redux";
 import classes from "./Nav.module.css";
 import NavItem from "./NavItem/NavItem";
 
+
 const Nav = () => {
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
+
   return (
-   
-      <ul className={classes.Nav}>
-        <NavItem url="/" exact>Builder</NavItem>
-        <NavItem url="/orders">Orders</NavItem>
-        <NavItem url="/s">Spiner</NavItem>
-      </ul>
-   
+    <ul className={classes.Nav}>
+      <NavItem url="/" exact>Builder</NavItem>
+      { !isAuthenticated ? <NavItem url="/auth">Login</NavItem> : null}
+      { isAuthenticated ? <NavItem url="/orders">Orders</NavItem> : null}
+      { isAuthenticated ? <NavItem url="/logout">Logout</NavItem> : null}
+    </ul>
   );
 }
 
 export default Nav;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import classes from "./Nav.module.css";
+// import NavItem from "./NavItem/NavItem";
+
+// const Nav = () => {
+//   return (
+   
+//       <ul className={classes.Nav}>
+//         <NavItem url="/" exact>Builder</NavItem>
+//         <NavItem url="/orders">Orders</NavItem>
+//         <NavItem url="/s">Load</NavItem>
+//       </ul>
+   
+//   );
+// }
+
+// export default Nav;
