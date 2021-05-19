6fa8b43e62d1976b1640e168aae5a167d0ae5821
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-19 17:48:30 +0600

    Add logout page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Auth from './components/Auth/Auth';
 import Checkout from './components/Checkout/Checkout';
 import DonutBuilder from './components/DonutBuilder/DonutBuilder';
 import Layout from './components/Layout/Layout';
+import Logout from './components/Logout/Logout';
 import Orders from './components/Orders/Orders';
 import Loading from './Loading/Loading';
 
@@ -20,6 +21,7 @@ import Loading from './Loading/Loading';
            <Route path="/checkout" component={Checkout}/>
            <Route path="/orders" component={Orders}/>
            <Route path="/auth" component={Auth} />
+           <Route path="/logout" component={Logout} />
            <Route path="/s" component={Loading}/>
            <Redirect to="/"/>
          </Switch>
---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+// import { logout }
+
+import { useDispatch } from "react-redux";
+import { logout } from "../../store/action/auth";
+
+export default ({ history }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    logout(dispatch);
+    history.replace("/");
+  }, [dispatch, history]);
+
+  return null;
+}
