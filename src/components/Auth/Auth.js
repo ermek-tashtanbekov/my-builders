b350fd4867d605784c296906bb27a8de92404684
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-05-21 15:05:21 +0600

    COPY AND PAST

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -15,10 +15,14 @@ export default withAxios(() => {
   const { loading, error, token } = useSelector(state => state.auth);
   const location = useLocation();
 
+
+
   const formSubmitted = (event) => {
     start(dispatch);
 
     const data = new FormData(event.target);
+    const method = event.nativeEvent.submitter.innerText == "Sign in"
+      ? "signin" : "signup";
     auth(dispatch, method, data.get('email'), data.get('password'));
 
     event.preventDefault();
@@ -27,15 +31,12 @@ export default withAxios(() => {
   let formOutput = <Loading/>;
   if (!loading) {
     formOutput = (
-      <form onSubmit={formSubmitted} className={classes.form}>
+      <form onSubmit={formSubmitted}>
         <h1>Welcome</h1>
-        <input type="email" placeholder="E-mail" name="email" required  className={classes.inputone}/>
+        <input type="email" placeholder="E-mail" name="email" required />
         <input type="password" placeholder="Password" name="password" required minLength="6" />
-        <div className={classes.Button}>
-        <Button click={() => setMethod('signin')} green>Sign in</Button>
-        <Button click={() => setMethod('signup')} red>Sign up</Button>
-        </div>
-        
+        <Button>Sign in</Button>
+        <Button>Sign up</Button>
       </form>
     );
   }
@@ -59,3 +60,65 @@ export default withAxios(() => {
     </div>
   );
 }, axios);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   const formSubmitted = (event) => {
+//     start(dispatch);
+
+//     const data = new FormData(event.target);
+//     auth(dispatch, method, data.get('email'), data.get('password'));
+
+//     event.preventDefault();
+//   }
+
+//   let formOutput = <Loading/>;
+//   if (!loading) {
+//     formOutput = (
+//       <form onSubmit={formSubmitted} className={classes.form}>
+//         <h1>Welcome</h1>
+//         <input type="email" placeholder="E-mail" name="email" required  className={classes.inputone}/>
+//         <input type="password" placeholder="Password" name="password" required minLength="6" />
+//         <div className={classes.Button}>
+//         <Button click={() => setMethod('signin')} green>Sign in</Button>
+//         <Button click={() => setMethod('signup')} red>Sign up</Button>
+//         </div>
+        
+//       </form>
+//     );
+//   }
+
+//   let errorOutput = null;
+//   if (error) {
+//     errorOutput = <h4 className={classes.error}>{error.message}</h4>;
+//   }
+
+//   const [,redirect] = location.search.split('?');
+//   let redirectOutput = null;
+//   if (token !== null) {
+//     redirectOutput = <Redirect to={"/" + redirect ? redirect : ""} />;
+//   }
+
+//   return (
+//     <div className={classes.Auth}>
+//       {errorOutput}
+//       {formOutput}
+//       {redirectOutput}
+//     </div>
+//   );
+// }, axios);
